7f7ad50924087f3e0ccc3f1b99530fa244779a88
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2022-06-16 20:21:02 -0700

    Create Receiver.ino

---
+++ Receiver/Receiver.ino
@@ -0,0 +1,12 @@
+/*
+Created by Maureen Sanchez, Andres Zamudio, and Manuel Aboite
+Reference Code is cited in the README file of the repository. 
+*/
+
+void setup() {
+    
+}
+
+void loop() {
+    
+}
