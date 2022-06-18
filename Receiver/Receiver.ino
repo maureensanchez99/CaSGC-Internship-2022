0e110b43133678b5690d2c5308c80c3e35bb9c2e
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2022-06-17 22:32:55 -0700

    Update Receiver.ino

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -3,10 +3,22 @@ Created by Maureen Sanchez, Andres Zamudio, and Manuel Aboite
 Reference Code is cited in the README file of the repository. 
 */
 
+#include <SPI.h>
+#include "RF24.h"
+RF24 radio(9,10);
+
+//constants 
+const uint64_t address = 0xF0F0F0F000LL; //address 
+double msg[4]; 
+
 void setup() {
-    
+  radio.begin(); //starts the radio module
+  radio.openReadingPipe(1, address); //opens connections between the radio modules
+  radio.startListening(); //reciever begins waiting for commands from transmitter Arduino   
 }
 
 void loop() {
-    
+  if (radio.available()){ //checks to see if radio is able to connect
+    radio.read(msg,1); //if so start recieving messages to perform
+  }
 }
