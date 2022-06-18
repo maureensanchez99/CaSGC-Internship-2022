7ddd8ff660cf63851290b569aa7462439968adb7
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2022-06-17 22:34:28 -0700

    Update Transmitter.ino

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -3,10 +3,19 @@ Created by Maureen Sanchez, Andres Zamudio, and Manuel Aboite
 Reference Code is cited in the README file of the repository. 
 */
 
+#include <SPI.h>
+#include "RF24.h"
+RF24 radio(9,10);
+
+//constants 
+const uint64_t address = 0xF0F0F0F000LL; //address 
+double msg[4]; //message that is being sent to the Aruino receiver with four different data values
+
 void setup() {
-    
+  radio.begin(); //starts the radio module 
+  radio.openWritingPipe(address); //looks for reviever module at certain address
 }
 
 void loop() {
-    
+  radio.write(msg, 4);
 }
