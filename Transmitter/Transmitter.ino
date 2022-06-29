ba4565309c8f3d8ae08db13dbc3f96c59ee581ab
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2022-06-29 14:17:07 -0700

    Update Transmitter.ino

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -1,16 +1,20 @@
 /*
 Created by Maureen Sanchez, Andres Zamudio, and Manuel Aboite
-Reference Code is cited in the README file of the repository. 
+Reference Code is cited in the README file of the repository
+Purpose: needs to measure heart rate, blood oxygen levels, and recored the ECG/EKG
 */
 
-// needs to measure heart rate, blood oxygen levels, and recored the ECG/EKG
-
 #include <SPI.h>
+#include <nRF24L01.h>
 #include "RF24.h"
-RF24 radio(9,10);
+
+/*connected to digital pins (pins with a ~)
+used for setting module into standby or active mode
+switching between transmit or command mode */
+RF24 radio(9,10); 
 
 //constants 
-const uint64_t address = 0xF0F0F0F000LL; //address 
+const byte address = "00001"; //address 
 double msg[4]; //message that is being sent to the Aruino receiver with four different data values
 
 void setup() {
@@ -19,5 +23,9 @@ void setup() {
 }
 
 void loop() {
-  radio.write(msg, 4);
+  //gathers measurements to be sent to other Arduino
+  // *MAGIC*
+  
+  radio.write(msg, 4); //sends data to other Arduino
+  delay(1000); //delays gathering new data for one second then it repeats the loop
 }
